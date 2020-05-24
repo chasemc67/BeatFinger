fde419c3a259083c127cf351c05144b835560f08
Chase McCarty <chasemc67@gmail.com>
2020-05-23 21:25:54 -0700

    Added pinchScaling

--- Assets/Scripts/pinchMovement.cs
+++ Assets/Scripts/pinchMovement.cs
@@ -1,15 +1,33 @@
-namespace BeatFinger.Assets.Scripts
-{
-    public class pinchMovement
-    {
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BeatFinger.Assets.Script {
+    public class pinchMovement {
+
+        public Transform GameSpace;
+        private OVRHand leftHand;
+        private OVRHand rightHand;
+    
+        void Start()
+        {
+            leftHand = GameObject.Find("OVRCameraRig/TrackingSpace/LeftHandAnchor/OVRHandPrefab").GetComponent<OVRHand>();
+            rightHand = GameObject.Find("OVRCameraRig/TrackingSpace/RightHandAnchor/OVRHandPrefab").GetComponent<OVRHand>();
+
+            
+        }
         void Update()
-    {
-        //check for middle finger pinch on the left hand, and make che cube red in this case
-        // if (rightHand.GetFingerIsPinching(OVRHand.HandFinger.Middle)) {
-        //     m_renderer.material.color = Color.red;
-        // } else if (m_renderer.material.color == Color.red) {
-        //     m_renderer.material.color = Color.white;
-        // }
-    }
+        {
+            //check for middle finger pinch on the left hand, and make che cube red in this case
+            if (rightHand.GetFingerIsPinching(OVRHand.HandFinger.Index) || leftHand.GetFingerIsPinching(OVRHand.HandFinger.Index)) {
+                GameSpace.localScale += new Vector3(1f,1f,1f);
+            }
+
+            if (leftHand.GetFingerIsPinching(OVRHand.HandFinger.Index)) {
+                GameSpace.localScale -= new Vector3(1f,1f,1f);
+            }
+        }
     }
 }
---
+++ Assets/Scripts/pinchScaling.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BeatFinger.Assets.Script {
+    public class pinchScaling : MonoBehaviour {
+
+        public Transform GameSpace;
+        private OVRHand leftHand;
+        private OVRHand rightHand;
+
+        private Boolean pinching = false;
+
+        // distance between hands when the pinch started
+        private float pinchStartedDist = 0f;
+    
+        void Start()
+        {
+            leftHand = GameObject.Find("OVRCameraRig/TrackingSpace/LeftHandAnchor/OVRHandPrefab").GetComponent<OVRHand>();
+            rightHand = GameObject.Find("OVRCameraRig/TrackingSpace/RightHandAnchor/OVRHandPrefab").GetComponent<OVRHand>();
+
+            
+        }
+        void Update()
+        {
+
+            if (pinching) {
+                //check for middle finger pinch on the left hand, and make che cube red in this case
+                if (rightHand.GetFingerIsPinching(OVRHand.HandFinger.Index) && leftHand.GetFingerIsPinching(OVRHand.HandFinger.Index)) {
+                    float newPinchDistance = Vector3.Distance(leftHand.GetComponent<Transform>().position, rightHand.GetComponent<Transform>().position);
+                    GameSpace.localScale *= 0.5f + Sigmoid(newPinchDistance - pinchStartedDist);
+                    pinchStartedDist = newPinchDistance;
+                } else {
+                    stopPinching();
+                }    
+            } else {
+                if (rightHand.GetFingerIsPinching(OVRHand.HandFinger.Index) && leftHand.GetFingerIsPinching(OVRHand.HandFinger.Index)) {
+                    startPinching();
+                }
+            }
+        }
+
+        void startPinching() {
+            pinching = true;
+            pinchStartedDist =  Vector3.Distance(leftHand.GetComponent<Transform>().position, rightHand.GetComponent<Transform>().position);
+        }
+
+        void stopPinching() {
+            pinching = false;
+        }
+
+        public static float Sigmoid(float value) {
+            float k = (float)Math.Exp((double)value);
+            return k / (1.0f + k);
+        }
+    }
+}

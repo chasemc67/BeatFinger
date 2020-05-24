f485c9606f4d05b49f6945d92003f7fe6b839d6c
Chase McCarty <chasemc67@gmail.com>
2020-05-23 21:40:50 -0700

    Added movement

--- Assets/Scripts/pinchMovement.cs
+++ Assets/Scripts/pinchMovement.cs
@@ -5,29 +5,60 @@ using UnityEngine;
 using UnityEngine.UI;
 
 namespace BeatFinger.Assets.Script {
-    public class pinchMovement {
+    public class pinchMovement : MonoBehaviour {
 
         public Transform GameSpace;
         private OVRHand leftHand;
         private OVRHand rightHand;
+
+        private Boolean pinching = false;
+
+        // distance between hands when the pinch started
+        private Vector3 pinchStartedLoc = new Vector3(0f, 0f, 0f);
+        private OVRHand pinchHand;
     
         void Start()
         {
             leftHand = GameObject.Find("OVRCameraRig/TrackingSpace/LeftHandAnchor/OVRHandPrefab").GetComponent<OVRHand>();
             rightHand = GameObject.Find("OVRCameraRig/TrackingSpace/RightHandAnchor/OVRHandPrefab").GetComponent<OVRHand>();
-
-            
         }
         void Update()
         {
-            //check for middle finger pinch on the left hand, and make che cube red in this case
-            if (rightHand.GetFingerIsPinching(OVRHand.HandFinger.Index) || leftHand.GetFingerIsPinching(OVRHand.HandFinger.Index)) {
-                GameSpace.localScale += new Vector3(1f,1f,1f);
+
+            if (pinching) {
+                //check for middle finger pinch on the left hand, and make che cube red in this case
+                if ((rightHand.GetFingerIsPinching(OVRHand.HandFinger.Index) || leftHand.GetFingerIsPinching(OVRHand.HandFinger.Index)) && !(rightHand.GetFingerIsPinching(OVRHand.HandFinger.Index) && leftHand.GetFingerIsPinching(OVRHand.HandFinger.Index))) {
+                    GameSpace.position += (pinchHand.GetComponent<Transform>().position - pinchStartedLoc);
+                    pinchStartedLoc = pinchHand.GetComponent<Transform>().position;
+                } else {
+                    stopPinching();
+                }    
+            } else {
+                if ((rightHand.GetFingerIsPinching(OVRHand.HandFinger.Index) || leftHand.GetFingerIsPinching(OVRHand.HandFinger.Index)) && !(rightHand.GetFingerIsPinching(OVRHand.HandFinger.Index) && leftHand.GetFingerIsPinching(OVRHand.HandFinger.Index))) {
+                    startPinching();
+                }
+            }
+        }
+
+        void startPinching() {
+            pinching = true;
+
+            if (rightHand.GetFingerIsPinching(OVRHand.HandFinger.Index)) {
+                pinchHand = rightHand;
+            } else {
+                pinchHand = leftHand;
             }
 
-            if (leftHand.GetFingerIsPinching(OVRHand.HandFinger.Index)) {
-                GameSpace.localScale -= new Vector3(1f,1f,1f);
-            }
+            pinchStartedLoc =  pinchHand.GetComponent<Transform>().position;
+        }
+
+        void stopPinching() {
+            pinching = false;
+        }
+
+        public static float Sigmoid(float value) {
+            float k = (float)Math.Exp((double)value);
+            return k / (1.0f + k);
         }
     }
 }
